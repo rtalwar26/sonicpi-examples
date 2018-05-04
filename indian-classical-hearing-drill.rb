af3336430415c6c5b08f32f95d9b298db798ace7
Rajat Talwar <cuterajat26@gmail.com>
2018-05-04 11:07:08 +0530

    add more notes, and skip invalid intervals

--- indian-classical-hearing-drill.rb
+++ indian-classical-hearing-drill.rb
@@ -1,6 +1,7 @@
 
-samps = "/path/to/harmonium"
+samps = "/Users/rohittalwar/personal/sonic/harmonium"
 use_bpm 60
+
 define :harm do |n|
   print n
   sample_length = 2;
@@ -9,19 +10,35 @@ define :harm do |n|
   sleep (sample_length*finish_length)
 end
 live_loop :myloop do
-  use_random_seed 2450 #
-  nring = (ring "Re_M","Sa_B","Ni_B","Dha_B","Pa_B");
+  
+  use_random_seed 3701 #
+  invalid = {
+    "Pa_B"=>"Ga_M",
+    "Pa_B"=>"Ma_M",
+    "Dha_B"=>"Ma_M",
+    "Ni_B"=>"Ma_M",
+  }
+  
+  nring = (ring "Ma_M","Ga_M","Re_M","Sa_B","Ni_B","Dha_B","Pa_B");
   firstNote = "Sa_B"
   a = [];
   ##| a = [firstNote];
   ##| harm firstNote;
   sample :tabla_ghe5
+  note1 = nring.choose
+  note2 = nring.choose
+  if invalid[note1] == note2 || invalid[note2] == note1
+    print "invalid"
+    note1 = "Sa_B"
+    note2 = "Ma_M"
+  end
+  selectnotes = (ring note1,note2)
   2.times do
     
-    note = nring.choose
+    note = selectnotes.tick
     harm note;
     ##| sample (ring :tabla_ghe5,:tabla_te_m).tick
-    a << note
+    ##| a << note
     
   end
   sleep 0.2
